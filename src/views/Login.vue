683e9cfcce52a0a96bd593e7ed10d1bec50ce196
Ionut Cristian Paraschiv <ionut@3angle.tech>
2022-09-26 17:22:07 +0300

    Login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,52 @@
 <template>
-  <div class="vue-template">
-    <form @submit.prevent="login">
-      <h3>Sign In</h3>
-
-      <div class="form-group">
-        <label>Email address</label>
-        <input type="email" class="form-control form-control-lg" />
+<section>
+    <div class="page-header min-vh-100">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+            <div class="card card-plain">
+              <div class="card-header" style="box-shadow: none">
+                <p class="mb-0">Enter your email and password to sign in</p>
+              </div>
+              <div class="card-body">
+                <form role="form">
+                  <div class="mb-3">
+                    <input type="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
+                  </div>
+                  <div class="mb-3">
+                    <input type="email" class="form-control form-control-lg" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
+                  </div>
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                  </div>
+                  <div class="text-center">
+                    <button type="button" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0" @click="login()">Sign in</button>
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                <p class="mb-4 text-sm mx-auto">
+                  <router-link to="/forgot-password">Forgot password ?</router-link>
+                  <!-- <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a> -->
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
+              <img src="../assets/img/shapes/pattern-lines.svg" alt="pattern-lines" class="position-absolute opacity-4 start-0">
+              <div class="position-relative">
+                <img class="max-width-500 w-100 position-relative z-index-2" src="../assets/img/illustrations/chat.png">
+              </div>
+              <h4 class="mt-5 text-white font-weight-bolder">"Something cool about ReaderBench"</h4>
+              <p class="text-white">Cool description of what we do.</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control form-control-lg" />
-      </div>
-
-      <button
-        class="btn btn-dark btn-lg btn-block"
-        @click="login()"
-      >
-        Sign In
-      </button>
-
-      <p class="forgot-password text-right mt-2 mb-4">
-        <router-link to="/forgot-password">Forgot password ?</router-link>
-      </p>
-    </form>
-  </div>
+    </div>
+  </section>
 </template>
  
 <script>
