a7b6f3b894f1e95d6231a123e375c0a037ce3f76
Ionut Cristian Paraschiv <ionut@3angle.tech>
2022-09-30 12:02:36 +0300

    About page & Nav

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,16 @@ import './assets/css/nucleo-icons.css';
 import './assets/css/nucleo-svg.css';
 import './assets/css/soft-design-system.css?v=1.0.9';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+library.add(faFacebook);
+library.add(faInstagram);
+library.add(faTwitter);
+library.add(faGithub);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
