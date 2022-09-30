78186008723e6fc36e4097e3ae580347da880a85
Ionut Cristian Paraschiv <ionut@3angle.tech>
2022-09-30 10:28:22 +0300

    Moved footer within separate component

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,17 @@
       </div>
     </div>
     <router-view />
+    <Footer />
   </div>
 </template>
 <script>
   import Nav from './components/partials/Nav.vue';
+  import Footer from './components/partials/Footer.vue';
   export default {
     name: 'app',
     components: {
       Nav,
+      Footer,
     },
   };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -87,22 +87,6 @@
       </div>
     </div>
   </section>
-
-  <footer class="footer pt-5 mt-5">
-    <hr class="horizontal dark mb-5">
-    <div class="container">
-      <div class=" row">
-        <div class="col-12">
-          <div class="text-center">
-            <p class="my-4 text-sm">
-              All rights reserved. Copyright © 2022 ReaderBench.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
 </template>
 <script>
   import ExperimentList from '../components/ExperimentList';
