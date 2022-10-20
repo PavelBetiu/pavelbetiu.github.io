5a3f39ec25d366d3655c743da72c4e1ab22691f9
Ionut Cristian Paraschiv <ionut@3angle.tech>
2022-10-20 18:05:20 +0300

    #14 Added table widget

--- src/components/experiments/cscl/CsclResult.vue
+++ src/components/experiments/cscl/CsclResult.vue
@@ -1,12 +1,28 @@
 <template>
-  <h1>CSCL Results Component (TODO)</h1>
+  <div class="row">
+    <div class="col-md-12">
+      <Table :data="tableInput" />
+    </div>
+  </div>
 </template>
 
 <script>
+import Table from "@/components/widgets/Table";
+import { CsclResult } from "@/data-objects/cscl-result";
+import { convertToContributionsTable } from "./cscl-converters";
+
 export default {
-  props: ["data"],
+  components: {
+    Table,
+  },
+  props: {
+    data: CsclResult,
+  },
   data() {
     return {};
+  },
+  created() {
+    this.tableInput = convertToContributionsTable(this.data);
   },
 };
 </script>
