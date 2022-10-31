d8ecd25d61b6a2ae9eb77eb28810796cc433ffb5
PavelBetiu <betiupavel@gmail.com>
2022-10-31 20:07:11 +0200

    #3 Map collab world on construction

--- src/components/widgets/async-world-map.vue
+++ src/components/widgets/async-world-map.vue
@@ -1,11 +1,9 @@
 <template>
 <b-card no-body>
-  <b-card-body class="pt-0">
-    <async-world-map class="map map-big"
-                     :map-data="mapData"
-                     :points="points">
-    </async-world-map>
-  </b-card-body>
+    <b-card-body class="pt-0">
+        <async-world-map class="map map-big" :map-data="mapData" :points="points">
+        </async-world-map>
+    </b-card-body>
 </b-card>
 </template>
 
@@ -14,53 +12,17 @@
 </style>
 
 <script>
-import AsyncWorldMap from '@/components/widgets/async-world-map.vue';
-
-    export default {
-    components: {
-        AsyncWorldMap
-    },
-    data() {
-        return {
-            mapData: {
-                AUS: 760,
-                BRA: 900,
-                CAN: 120,
-                DEU: 1300,
-                FRA: 540,
-                GBR: 690,
-                GEO: 200,
-                IND: 200,
-                ROU: 600,
-                RUS: 757,
-                USA: 1800
-            },
-            points: [{
-                    name: 'Brazil',
-                    centered: 'BRA',
-                },
-                {
-                    name: 'France',
-                    centered: 'FRA',
-                },
-                {
-                    name: 'Italy',
-                    centered: 'ITA',
-                },
-                {
-                    name: 'Oman',
-                    centered: 'OMN',
-                },
-                {
-                    name: 'Indonesia',
-                    centered: 'IDN',
-                },
-                {
-                    name: 'Romania',
-                    centered: 'ROU',
-                }
-            ]
-        };
+export default {
+    name: 'AsyncWorldMap',
+    props: {
+        mapData: {
+            type: Object,
+            required: true
+        },
+        points: {
+            type: Array,
+            required: true
+        }
     }
 };
 </script>
