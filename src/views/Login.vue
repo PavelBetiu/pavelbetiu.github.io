0fea9624a2aeb728c4fd60c85d7edca19f430797
PavelBetiu <betiupavel@gmail.com>
2023-02-14 18:13:34 +0200

    Small changes of pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,10 @@
 <template>
 <section>
+  <div class="background-login">
     <div class="page-header min-vh-100">
       <div class="container">
         <div class="row">
-          <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+          <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto background-card">
             <div class="card card-plain">
               <div class="card-header" style="box-shadow: none">
                 <p class="mb-0">Enter your email and password to sign in</p>
@@ -34,21 +35,29 @@
             </div>
           </div>
           <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
-              <img src="../assets/img/shapes/pattern-lines.svg" alt="pattern-lines" class="position-absolute opacity-4 start-0">
+           <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
+              <!-- <img src="../assets/img/shapes/pattern-lines.svg" alt="pattern-lines" class="position-absolute opacity-4 start-0"> -->
               <div class="position-relative">
-                <img class="max-width-500 w-100 position-relative z-index-2" src="../assets/img/illustrations/chat.png">
+                <img class="max-width-500 w-80 h-80 position-relative z-index-2" src="../assets/images/readerbench-small.svg">
               </div>
-              <h4 class="mt-5 text-white font-weight-bolder">"Something cool about ReaderBench"</h4>
-              <p class="text-white">Cool description of what we do.</p>
+              <!-- <h4 class="mt-5 text-white font-weight-bolder">"Something cool about ReaderBench"</h4>
+              <p class="text-white">Cool description of what we do.</p> -->
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   </section>
 </template>
-
+<style>
+.background-login {
+  background-color: #f8f9fa
+}
+.background-card {
+  background-color: #fff;
+}
+</style>
 <script>
 import auth from "@/services/auth";
 import axios from "axios";
