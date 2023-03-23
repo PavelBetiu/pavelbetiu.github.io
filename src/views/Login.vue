8aa796c1aa0c7bfdc78b883a318572d57cebdfdd
ManuTrt <emanuelthegamer@gmail.com>
2023-03-23 16:26:12 +0200

    #67 errors fixed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,8 @@
 <template>
-<section>
-  <div class="background-login">
-    <div class="page-header min-vh-100">
+  <div class="body page-header min-vh-100">
       <div class="container">
-        <div class="row">
-          <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto background-card">
+        <div class="row d-flex justify-content-around">
+          <div class="col-4 card">
             <div class="card card-plain">
               <div class="card-header" style="box-shadow: none">
                 <p class="mb-0">Enter your email and password to sign in</p>
@@ -34,30 +32,21 @@
               </div>
             </div>
           </div>
-          <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-           <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
-              <!-- <img src="../assets/img/shapes/pattern-lines.svg" alt="pattern-lines" class="position-absolute opacity-4 start-0"> -->
-              <div class="position-relative">
-                <img class="max-width-500 w-80 h-80 position-relative z-index-2" src="../assets/images/readerbench-small.svg">
-              </div>
-              <!-- <h4 class="mt-5 text-white font-weight-bolder">"Something cool about ReaderBench"</h4>
-              <p class="text-white">Cool description of what we do.</p> -->
-            </div>
+          <div class="col-4 d-flex justify-content-center align-items-center">
+            <img class="w-80 h-80" src="../assets/images/readerbench-small.svg">
           </div>
         </div>
       </div>
     </div>
-  </div>
-  </section>
 </template>
-<style>
-.background-login {
-  background-color: #f8f9fa
-}
-.background-card {
-  background-color: #fff;
+
+<style lang="scss" scoped>
+.body {
+    background-color: #f8f9fa;
 }
 </style>
+
+
 <script>
 import auth from "@/services/auth";
 import axios from "axios";
