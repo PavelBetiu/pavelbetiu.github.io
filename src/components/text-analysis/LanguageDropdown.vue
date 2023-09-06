b944f00b9dd81dd04b3693ed2ea251229c3516c1
ManuTrt <emanuelthegamer@gmail.com>
2023-09-06 15:13:45 +0300

    #89 done refactoring

--- src/components/text-analysis/LanguageDropdown.vue
+++ src/components/text-analysis/LanguageDropdown.vue
@@ -22,55 +22,27 @@
                 <label>Available tasks:</label>
             </div>
         </div>
-        <div class="row w-100">
-            <div class="col-12">
-                <select class="form-control" name="language-button" id="language-button" v-model="langID">
-                    <option value="-1" disabled selected>Click to select</option>
-                    <option v-for="{ id, label } of languages" :key="id" :value="id" >{{ label }}</option>
-                </select>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="row w-100">
-        <div class="col-12 ml-3">
-            <label>Available tasks:</label>
+    
+        <div class="list-group p-3">
+            <a v-for="{id, name, isActive} of tasksByLanguage" :key="id" href="javascript:;" @click="selectTask(id)" :class="'list-group-item list-group-item-action ' + active(isActive)">
+                {{ name }}
+            </a>
         </div>
     </div>
-
-    <div class="list-group p-3">
-        <a v-for="{id, name, isActive} of tasksByLanguage" :key="id" href="javascript:;" @click="selectTask(id)" :class="'list-group-item list-group-item-action ' + active(isActive)">
-            {{ name }}
-        </a>
-    </div>
-</div>
-</template>
-
-<style>
-
-</style>
-
-<script>
-export default {
-    name: 'LanguageDropdown',
-    props: {
-        languages: Array,
-        tasks: Array,
-        onTaskSelect: Function,
-        onLanguageSelect: Function
-    },
-    data() {
-        return {
-            langID: -1,
-            selectedTaskId: -1
-        }
-    },
-    methods: {
-        selectTask(id) {
-            this.selectedTaskId = id;
-
-            this.onTaskSelect(id);
+    </template>
+    
+    <style>
+    
+    </style>
+    
+    <script>
+    export default {
+        name: 'LanguageDropdown',
+        props: {
+            languages: Array,
+            tasks: Array,
+            onTaskSelect: Function,
+            onLanguageSelect: Function
         },
         data() {
             return {
@@ -78,37 +50,51 @@ export default {
                 selectedTaskId: -1
             }
         },
-    },
-    computed: {
-        tasksByLanguage() {
-            let tasks = [];
-            this.tasksWithSelectedStatus.forEach(task => {
-                if (task['languages'].includes(this.langID)) {
-                    tasks.push(task);
+        methods: {
+            selectTask(id) {
+                this.selectedTaskId = id;
+    
+                this.onTaskSelect(id);
+            },
+            active(isActive) {
+                if (isActive) {
+                    return "active";
+                } else {
+                    return "";
                 }
             },
         },
-        tasksWithSelectedStatus() {
-            let tasks = [];
-            this.tasks.forEach(task => {
-                let taskWithSelectedStatus =
-                {
-                    id: task.id,
-                    name: task.name,
-                    isActive: task.id === this.selectedTaskId,
-                    languages: task.languages
-                }
-                tasks.push(taskWithSelectedStatus);
-            });
-
-            return tasks;
-        }
-    },
-    watch: {
-        langID: function (newVal, oldVal) {
-            this.selectedTaskId = -1;
-            this.onLanguageSelect(newVal);
-        }
-    },
-};
-</script>
+        computed: {
+            tasksByLanguage() {
+                let tasks = [];
+                this.tasksWithSelectedStatus.forEach(task => {
+                    if (task['languages'].includes(this.langID)) {
+                        tasks.push(task);
+                    }
+                });
+                return tasks;
+            },
+            tasksWithSelectedStatus() {
+                let tasks = [];
+                this.tasks.forEach(task => {
+                    let taskWithSelectedStatus =
+                    {
+                        id: task.id,
+                        name: task.name,
+                        isActive: task.id === this.selectedTaskId,
+                        languages: task.languages
+                    }
+                    tasks.push(taskWithSelectedStatus);
+                });
+    
+                return tasks;
+            }
+        },
+        watch: {
+            langID: function (newVal, oldVal) {
+                this.selectedTaskId = -1;
+                this.onLanguageSelect(newVal);
+            }
+        },
+    };
+    </script>
--- src/components/text-analysis/TaskInputForm.vue
+++ src/components/text-analysis/TaskInputForm.vue
@@ -1,245 +1,133 @@
 <template>
-<div id="textAnalysisForm" class="card">
-<<<<<<< HEAD
-    <div class="card-header text-center pt-4 pb-3 d-flex justify-content-center">
-        <span class="badge rounded-pill bg-light text-dark">{{ task['name'] }}</span>
-    </div>
-    <div class="card-body text-lg-left text-center px-5 pb-2">
-        <div class="form-group">
-            <textarea v-if="!isLoading && !doneProcessing" class="form-control" id="textInputBox" rows="15" v-model="text" placeholder="Please insert your text here..."></textarea>
-            <textarea v-else-if="isLoading" class="form-control" id="textInputBoxDisabled" rows="15" v-model="text" disabled></textarea>
-            <!-- Display Highlighted Text -->
-            <div v-else-if="doneProcessing" class="card-body text-lg-left text-center px-5 pb-2">
-                <h3 class="mb-1">Highlighted Text:</h3>
-                <div v-html="highlightedText"></div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Display Keywords -->
-    <div v-if="doneProcessing" class="card-body text-lg-left text-center px-5 pb-2">
-        <h3 class="mb-1">Keywords:</h3>
-        <div class="d-flex flex-wrap keywords-list">
-            <div v-for="keyword of keywords" :key="keyword" class="badge bg-secondary m-2">
-                {{ keyword }}
-            </div>
-        </div>
-    </div>
-
-    <div class="card-footer text-center pt-3 pb-1 px-5 d-flex justify-content-center">
-        <button v-if="!isLoading && !doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="process()">
-            Process
-        </button>
-        <button v-else-if="isLoading" type="button" class="btn bg-gradient-primary w-100" disabled>
-            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-            Processing...
-        </button>
-        <button v-else-if="doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="clear()">
-            New Process
-        </button>
-    </div>
-=======
-
-    <div class="card-header">
-        <slot name="over-header">
-        </slot>
-
-        <slot name="header">
-            <div class="text-center w-100 d-flex justify-content-center">
-                <span class="badge rounded-pill bg-light text-dark">{{ title }}</span>
-            </div>
-        </slot>
-
-        <slot name="sub-header">
-        </slot>
-    </div>
-
-    <div class="card-body">
-        <slot name="over-body">
-        </slot>
-        <slot name="body">
-            <div class="text-lg-left text-center px-2">
-                <div class="form-group">
-                    <textarea v-if="!isLoading && !doneProcessing" class="form-control" id="textInputBox" rows="15" v-model="text" placeholder="Please insert your text here..."></textarea>
-                    <textarea v-else-if="isLoading || doneProcessing" class="form-control" id="textInputBoxDisabled" rows="15" v-model="text" disabled></textarea>
-                </div>
-            </div>
-        </slot>
-        <slot name="sub-body">
-        </slot>
-    </div>
-
-    <div class="card-footer">
-        <slot name="over-footer">
-        </slot>
-        <slot name="footer">
-            <div class="w-100 d-flex justify-content-center">
-                <button v-if="!isLoading && !doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="process()">Process</button>
-
-                <button v-else-if="isLoading" type="button" class="btn bg-gradient-primary w-100" disabled>
-                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                    Processing...
-                </button>
-
-                <button v-else-if="doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="clear()">
-                    New Process
-                </button>
-            </div>
-        </slot>
-        <slot name="sub-footer">
-        </slot>
-    </div>
-
->>>>>>> 86-text-analysis-page-integrations-and-others
-</div>
-</template>
-
-<script>
-import {
-    LanguageDropdown
-} from '@/components/text-analysis/LanguageDropdown.vue';
-import {
-    inject
-} from 'vue';
-import {
-    TEXT_ANALYSIS_SERVICE
-} from '@/services/text-analysis-service.interface';
-export default {
-    name: 'TaskInputForm',
-    props: {
-<<<<<<< HEAD
-        task: Object,
-        langID: Number
-=======
-        title: {
-            type: String,
-            optional: false
-        },
-        processText: {
-            type: Function,
-            optional: false
-        },
-        clearForm: {
-            type: Function,
-            optional: true,
-            default: undefined
-        }
->>>>>>> 86-text-analysis-page-integrations-and-others
-    },
-    data() {
-        return {
-            text: '',
-            isLoading: false,
-            doneProcessing: false,
-<<<<<<< HEAD
-            taservice: inject(TEXT_ANALYSIS_SERVICE),
-            keywords: [],
-            highlightedText: '', // Added to store the highlighted text
-            keywordColors: {}, // Added to store colors for keywords
-=======
->>>>>>> 86-text-analysis-page-integrations-and-others
-        };
-    },
-    methods: {
-        clear() {
-            this.text = '';
-            this.doneProcessing = false;
-<<<<<<< HEAD
-        },
-        highlightKeywords() {
-            let highlightedText = this.text;
-
-            // Loop through keywords and apply highlighting
-            this.keywords.forEach((keyword) => {
-                const keywordParts = keyword.split(' ');
-
-                // Create a regular expression pattern to match keyword parts with any words in between
-                const keywordPattern = new RegExp(
-                    `(${keywordParts
-                .map((part) => `\\b${part}\\b`)
-                .join('.*?')})`,
-                    'gi'
-                );
-
-                highlightedText = highlightedText.replace(
-                    keywordPattern,
-                    (match) => `<span class="highlighted">${match}</span>`
-                );
-            });
-
-            this.highlightedText = highlightedText;
-        },
-        async process() {
-            this.isLoading = true;
-            console.log(this.text);
-            console.log(this.langID);
-            const response = await this.taservice.getKeywords({
-                text: this.text,
-                lang: this.langID
-            });
-            console.log('the get keywords response is:')
-            console.log(response);
-            this.keywords = response.keywords;
-            this.isLoading = false;
-            this.doneProcessing = true;
-            this.highlightKeywords();
-=======
-
-            if (this.clearForm !== undefined) {
-                this.clearForm();
-            }
-        },
-        async process() {
-            this.isLoading = true;
-
-            let valid = await this.processText(this.text);
-
-            this.isLoading = false;
-
-            if (valid) {
-                this.doneProcessing = true;
-            }
->>>>>>> 86-text-analysis-page-integrations-and-others
-        },
-    },
-};
-</script>
+    <div id="textAnalysisForm" class="card">
     
-<style>
-#textAnalysisForm>.card-header {
-    border-radius: var(--bs-card-inner-border-radius) var(--bs-card-inner-border-radius) 0 0;
-    box-shadow: 0 0.225em 0.25em rgb(10 10 10 / 10%);
-    z-index: 1;
-}
-
-#textAnalysisForm>.card-footer {
-    border-radius: 0 0 var(--bs-card-inner-border-radius) var(--bs-card-inner-border-radius);
-    box-shadow: 0 -0.225em 0.25em rgb(10 10 10 / 10%);
-    z-index: 1;
-}
-
-#textAnalysisForm>.card-body {
-    background-color: #f8f9fa;
-}
-
-<<<<<<< HEAD
-.keywords-list {
-    display: flex;
-    justify-content: center;
-    max-width: 100%;
-}
-
-* {
-    font-family: 'Roboto', sans-serif;
-}
-
-.highlighted {
-    background-color: yellow;
-    /* Adjust the highlighting style as needed */
-}
-=======
-* {
-    font-family: 'Roboto', sans-serif;
-}
->>>>>>> 86-text-analysis-page-integrations-and-others
-</style>
+        <div class="card-header">
+            <slot name="over-header">
+            </slot>
+    
+            <slot name="header">
+                <div class="text-center w-100 d-flex justify-content-center">
+                    <span class="badge rounded-pill bg-light text-dark">{{ title }}</span>
+                </div>
+            </slot>
+    
+            <slot name="sub-header">
+            </slot>
+        </div>
+    
+        <div class="card-body">
+            <slot name="over-body">
+            </slot>
+            <slot name="body">
+                <div class="text-lg-left text-center px-2">
+                    <div class="form-group">
+                        <textarea v-if="!isLoading && !doneProcessing" class="form-control" id="textInputBox" rows="15" v-model="text" placeholder="Please insert your text here..."></textarea>
+                        <textarea v-else-if="isLoading || doneProcessing" class="form-control" id="textInputBoxDisabled" rows="15" v-model="text" disabled></textarea>
+                    </div>
+                </div>
+            </slot>
+            <slot name="sub-body">
+            </slot>
+        </div>
+    
+        <div class="card-footer">
+            <slot name="over-footer">
+            </slot>
+            <slot name="footer">
+                <div class="w-100 d-flex justify-content-center">
+                    <button v-if="!isLoading && !doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="process()">Process</button>
+    
+                    <button v-else-if="isLoading" type="button" class="btn bg-gradient-primary w-100" disabled>
+                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                        Processing...
+                    </button>
+    
+                    <button v-else-if="doneProcessing" type="button" class="btn bg-gradient-primary w-100" @click="clear()">
+                        New Process
+                    </button>
+                </div>
+            </slot>
+            <slot name="sub-footer">
+            </slot>
+        </div>
+    
+    </div>
+    </template>
+    
+    <script>
+    import {
+        inject
+    } from 'vue';
+    import {
+        TEXT_ANALYSIS_SERVICE
+    } from '@/services/text-analysis-service.interface'
+    
+    export default {
+        name: 'TaskInputForm',
+        props: {
+            title: {
+                type: String,
+                optional: false
+            },
+            processText: {
+                type: Function,
+                optional: false
+            },
+            clearForm: {
+                type: Function,
+                optional: true,
+                default: undefined
+            }
+        },
+        data() {
+            return {
+                text: '',
+                isLoading: false,
+                doneProcessing: false,
+            };
+        },
+        methods: {
+            clear() {
+                this.text = '';
+                this.doneProcessing = false;
+    
+                if (this.clearForm !== undefined) {
+                    this.clearForm();
+                }
+            },
+            async process() {
+                this.isLoading = true;
+    
+                let valid = await this.processText(this.text);
+    
+                this.isLoading = false;
+    
+                if (valid) {
+                    this.doneProcessing = true;
+                }
+            },
+        },
+    };
+    </script>
+    
+    <style>
+    #textAnalysisForm>.card-header {
+        border-radius: var(--bs-card-inner-border-radius) var(--bs-card-inner-border-radius) 0 0;
+        box-shadow: 0 0.225em 0.25em rgb(10 10 10 / 10%);
+        z-index: 1;
+    }
+    
+    #textAnalysisForm>.card-footer {
+        border-radius: 0 0 var(--bs-card-inner-border-radius) var(--bs-card-inner-border-radius);
+        box-shadow: 0 -0.225em 0.25em rgb(10 10 10 / 10%);
+        z-index: 1;
+    }
+    
+    #textAnalysisForm>.card-body {
+        background-color: #f8f9fa;
+    }
+    
+    * {
+        font-family: 'Roboto', sans-serif;
+    }
+    </style>
