651412a087b95fe70f17eb29c0a8f1d1075295a8
PavelBetiu <betiupavel@gmail.com>
2023-09-20 14:34:03 +0300

    #98 added one member to the core team

--- src/views/People.vue
+++ src/views/People.vue
@@ -156,7 +156,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/traianrebedea.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Traian Rebedea</h4>
@@ -193,7 +193,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/irinatoma.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Irina Toma</h4>
@@ -230,7 +230,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/mariadorineladascalu.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Maria-Dorinela Dascalu</h4>
@@ -267,7 +267,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/ionutcristianparaschiv.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Ionut Cristian Paraschiv</h4>
@@ -287,7 +287,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/laurneagu.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Laurentiu Neagu</h4>
@@ -312,7 +312,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/dragoscorlatescu.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Dragos Corlatescu</h4>
@@ -349,7 +349,39 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
+                            <div class="p-3 text-center">
+                                <img src="../assets/images/anonymous.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
+                                <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Andrei Paraschiv</h4>
+                                <h5 class="mt-3">PhD Student</h5>
+                                <a href="https://scholar.google.com/citations?user=Hv_MLx0AAAAJ&hl=en" class="mr-4">
+                                    <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" viewBox="0 0 30 30.000001" preserveAspectRatio="xMidYMid meet" version="1.0">
+                                        <defs>
+                                            <clipPath id="id1">
+                                                <path d="M 3.386719 3 L 27.339844 3 L 27.339844 28 L 3.386719 28 Z M 3.386719 3 " clip-rule="nonzero" />
+                                            </clipPath>
+                                        </defs>
+                                        <g clip-path="url(#id1)">
+                                            <path fill="rgb(0%, 0%, 0%)" d="M 14.660156 3.25 L 3.386719 10.308594 L 11.125 10.308594 C 11.097656 10.417969 11.050781 10.515625 11.027344 10.625 C 10.960938 10.964844 10.910156 11.34375 10.910156 11.734375 C 10.910156 16.773438 16.054688 16.207031 16.054688 16.207031 L 16.054688 17.492188 C 16.054688 18.011719 16.734375 17.832031 16.816406 18.890625 C 16.476562 18.890625 9.691406 18.695312 9.691406 23.277344 C 9.691406 27.882812 15.679688 27.65625 15.679688 27.65625 C 15.679688 27.65625 22.59375 27.964844 22.59375 22.273438 C 22.597656 18.871094 18.636719 17.765625 18.636719 16.398438 C 18.636719 15.015625 21.621094 14.609375 21.621094 11.375 C 21.621094 9.960938 21.523438 8.953125 20.890625 8.238281 C 20.84375 8.1875 20.808594 8.152344 20.761719 8.121094 C 20.75 8.109375 20.738281 8.101562 20.726562 8.09375 L 20.898438 8.09375 L 23.816406 5.902344 L 23.816406 8.898438 C 23.816406 8.953125 23.820312 9.007812 23.832031 9.0625 C 23.609375 9.1875 23.429688 9.363281 23.300781 9.585938 C 23.171875 9.808594 23.109375 10.050781 23.113281 10.308594 L 23.113281 11.722656 C 23.109375 11.910156 23.144531 12.09375 23.214844 12.269531 C 23.285156 12.445312 23.386719 12.597656 23.515625 12.734375 C 23.648438 12.867188 23.804688 12.972656 23.976562 13.042969 C 24.152344 13.117188 24.332031 13.152344 24.519531 13.152344 C 24.710938 13.152344 24.890625 13.117188 25.066406 13.042969 C 25.238281 12.972656 25.390625 12.867188 25.523438 12.734375 C 25.65625 12.597656 25.757812 12.445312 25.828125 12.269531 C 25.898438 12.09375 25.933594 11.910156 25.929688 11.722656 L 25.929688 10.308594 C 25.933594 10.050781 25.871094 9.808594 25.742188 9.585938 C 25.613281 9.363281 25.433594 9.1875 25.207031 9.0625 C 25.21875 9.007812 25.226562 8.953125 25.226562 8.898438 L 25.226562 4.839844 L 27.339844 3.25 Z M 15.632812 7.5625 C 16.039062 7.542969 16.445312 7.640625 16.835938 7.863281 C 17.125 8.007812 17.402344 8.21875 17.644531 8.480469 C 18.148438 8.984375 18.570312 9.714844 18.796875 10.578125 C 19.332031 12.625 18.636719 14.597656 17.191406 14.96875 C 15.765625 15.375 14.171875 14.039062 13.621094 12.007812 C 13.378906 11.015625 13.410156 10.054688 13.6875 9.292969 C 13.691406 9.28125 13.695312 9.273438 13.699219 9.265625 C 13.703125 9.261719 13.710938 9.257812 13.714844 9.253906 C 13.792969 8.953125 13.921875 8.679688 14.082031 8.457031 C 14.371094 8.035156 14.753906 7.746094 15.226562 7.617188 C 15.363281 7.585938 15.496094 7.566406 15.632812 7.5625 Z M 16.183594 19.75 C 18.566406 19.570312 20.597656 20.886719 20.746094 22.675781 C 20.84375 24.449219 19.007812 26.027344 16.605469 26.1875 C 14.222656 26.351562 12.160156 25.050781 12.046875 23.277344 C 11.933594 21.492188 13.78125 19.929688 16.183594 19.75 Z M 16.183594 19.75 " fill-opacity="1" fill-rule="nonzero" />
+                                        </g>
+                                    </svg>
+                                </a>
+                                <a href="https://www.researchgate.net/profile/Andrei-Paraschiv" class="mr-4">
+                                    <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
+                                        <path d="M0 32v448h448V32H0zm262.2 334.4c-6.6 3-33.2 6-50-14.2-9.2-10.6-25.3-33.3-42.2-63.6-8.9 0-14.7 0-21.4-.6v46.4c0 23.5 6 21.2 25.8 23.9v8.1c-6.9-.3-23.1-.8-35.6-.8-13.1 0-26.1.6-33.6.8v-8.1c15.5-2.9 22-1.3 22-23.9V225c0-22.6-6.4-21-22-23.9V193c25.8 1 53.1-.6 70.9-.6 31.7 0 55.9 14.4 55.9 45.6 0 21.1-16.7 42.2-39.2 47.5 13.6 24.2 30 45.6 42.2 58.9 7.2 7.8 17.2 14.7 27.2 14.7v7.3zm22.9-135c-23.3 0-32.2-15.7-32.2-32.2V167c0-12.2 8.8-30.4 34-30.4s30.4 17.9 30.4 17.9l-10.7 7.2s-5.5-12.5-19.7-12.5c-7.9 0-19.7 7.3-19.7 19.7v26.8c0 13.4 6.6 23.3 17.9 23.3 14.1 0 21.5-10.9 21.5-26.8h-17.9v-10.7h30.4c0 20.5 4.7 49.9-34 49.9zm-116.5 44.7c-9.4 0-13.6-.3-20-.8v-69.7c6.4-.6 15-.6 22.5-.6 23.3 0 37.2 12.2 37.2 34.5 0 21.9-15 36.6-39.7 36.6z" /></svg>
+                                </a>
+                                <a href="https://dblp.dagstuhl.de/pid/253/9218.html" class="mr-4">
+                                    <svg width="30" height="30" style="color: #f3da35" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <title>dblp</title>
+                                        <path d="M3.075.002c-.096.013-.154.092-.094.31L4.97 7.73 3.1 8.6s-.56.26-.4.85l2.45 9.159s.16.59.72.33l6.169-2.869 1.3-.61s.52-.24.42-.79l-.01-.06-1.13-4.22-.658-2.45-.672-2.49v-.04s-.16-.59-.84-1L3.5.141s-.265-.16-.425-.139zM18.324 5.03a.724.724 0 0 0-.193.06l-5.602 2.6.862 3.2 1.09 4.08.01.06c.05.47-.411.79-.411.79l-1.88.87.5 1.89.04.1c.07.17.28.6.81.91l6.95 4.269s.68.41.52-.17l-1.981-7.4 1.861-.86s.56-.26.4-.85L18.85 5.42s-.116-.452-.526-.39z" fill="#f3da35"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                            <hr class="vertical dark">
+                        </div>
+
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="" />
                                 <img src="../assets/images/anonymous.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
@@ -359,7 +391,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/anonymous.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Mihai Masala</h4>
@@ -368,7 +400,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/razvanparoiu.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Razvan Paroiu</h4>
@@ -388,7 +420,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/anonymous.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Vlad Dumitru</h4>
@@ -397,7 +429,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/AlexDima2.jpg" class="img-fluid border-radius-lg"  width="200" height="200"/>
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Alexandru Dima</h4>
@@ -428,7 +460,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/emanueltertes.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Emanuel Tertes</h4>
@@ -442,7 +474,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/pavelbetiu.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Pavel Betiu</h4>
@@ -456,7 +488,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/Teo.jpg" class="img-fluid border-radius-lg" width="200" height="200" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Teodor Neacsu</h4>
@@ -465,7 +497,7 @@
                             <hr class="vertical dark">
                         </div>
 
-                        <div class="col-md-4 position-relative">
+                        <div class="col-md-3 position-relative">
                             <div class="p-3 text-center">
                                 <img src="../assets/images/andreeadutulescu.jpg" class="img-fluid border-radius-lg" />
                                 <h4 class="text-gradient text-primary"><span id="state1" countTo="70"></span>Andreea Dutulescu</h4>
